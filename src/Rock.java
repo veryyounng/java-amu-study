8fc6a6fc0425ea5d2e1841e9655715d3bec5a978
veryyounng <veryyoung811@gmail.com>
2024-02-24 01:30:23 +0900

    Solve: 돌계단 오르기 해결

--- src/Rock.java
+++ src/Rock.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Rock {
+import java.util.Scanner;
+
+public class Rock {
+    static int[] arr;
+    public int solution(int n){
+        arr[1] = 1;
+        arr[2] = 2;
+        for (int i = 3; i <= n+1; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n+1];
+    }
+    public static void main(String[] args) {
+        Rock T = new Rock();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        arr= new int[n+2];
+        System.out.println(T.solution(n));
+    }
 }
